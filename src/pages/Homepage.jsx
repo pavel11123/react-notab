c7c6afa60c7e65aecf3456e7ab5d4fc0ad2cf803
pavel11123 <korobko.11@inbox.ru>
2022-10-20 23:30:05 +0600

    start block

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -9,7 +9,7 @@ const Homepage = () => {
           <div className="start__container">
             <div className="start__block-wrapper">
               <div className="start__block-main d-fl">
-                <div className="start__block-left">
+                <div className="start__block-left d-fl-col">
                   <h1>
                     A platform
                     <br />
@@ -18,15 +18,30 @@ const Homepage = () => {
                   <p>
                     NoTab allows bars & nightclubs
                     <br />
-                    to manage, understand, and grow
-                    <br />
-                    their business from a single dashboard
+                    to manage, understand, and
+                    <br className="start__block-mobile" /> grow
+                    <br className="start__block-pc" />
+                    their business from <br className="start__block-mobile" />a
+                    single dashboard
                   </p>
                 </div>
                 <div className="start__block-right d-fl">
+                  <div className="start__element-gradient"></div>
+                  <div className="start__right-filter"></div>
                   <div className="start__block-right--img">
                     <img src="./img/photo/phone.webp" alt="phone" />
                   </div>
+                </div>
+              </div>
+              <div className="start__block-bottom d-fl-wrap">
+                <h3>Download our app</h3>
+                <div className="button__store d-fl-wrap">
+                  <a href="/" className="button__store-ios">
+                    <img src="./img/icon/app-store.svg" alt="app-store" />
+                  </a>
+                  <a href="/" className="button__store-google">
+                    <img src="./img/icon/google-play.svg" alt="app-store" />
+                  </a>
                 </div>
               </div>
             </div>
